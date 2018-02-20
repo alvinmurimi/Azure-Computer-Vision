fc59756f173518c4f5d93bd68943ff11e9419fd5
n1rg7_msf <alvynirgz@gmail.com>
2018-02-20 20:29:53 +0000

    Delete example.php

--- example.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'azure.php';
-
-$image_url="https://cutecats.com/wp-content/uploads/2015/03/lily_5-515x276.jpg";
-$azure=new Computer_Vision();
-$image_description=$azure->image($image_url);
-echo $image_description;
-?>
